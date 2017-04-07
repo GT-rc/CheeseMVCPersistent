23a0f956ab9b06dcce5060b021a8b9109dd87ffc
Glynnis Thomas <glynnisthomas@gmail.com>
2017-04-06 23:27:27 -0500

    Completed Part 1 of Studio Assignment: Added category controller and model, view and viewmodel for add feature. Migrated db.

--- src/CheeseMVC/Migrations/20170407041137_Category2Migration.Designer.cs
+++ src/CheeseMVC/Migrations/20170407041137_Category2Migration.Designer.cs
@@ -8,9 +8,10 @@ using CheeseMVC.Data;
 namespace CheeseMVC.Migrations
 {
     [DbContext(typeof(CheeseDbContext))]
-    partial class CheeseDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170407041137_Category2Migration")]
+    partial class Category2Migration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.1")
