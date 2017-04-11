07462aea5e7b21f3813a2ab855061bd8dc4ee9c5
Glynnis Thomas <glynnisthomas@gmail.com>
2017-04-11 00:19:26 -0500

    Added the Add Menu functionality

---
+++ src/CheeseMVC/Controllers/MenuController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CheeseMVC.ViewModels;
+using CheeseMVC.Data;
+using Microsoft.EntityFrameworkCore;
+using CheeseMVC.Models;
+
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CheeseMVC.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly CheeseDbContext context;
+
+        public MenuController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            IList<Menu> menus = context.Menus.Include(c => c.CheeseMenus).ToList();
+            return View(menus);
+        }
+
+        public IActionResult Add()
+        {
+            AddMenuViewModel addMenuViewModel = new AddMenuViewModel();
+            return View(addMenuViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddMenuViewModel addMenuViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Menu newMenu = new Menu
+                {
+                    Name = addMenuViewModel.Name
+                };
+
+                context.Menus.Add(newMenu);
+                context.SaveChanges();
+
+                return Redirect("/Menu/ViewMenu/" + newMenu.ID);
+            }
+
+            return View(addMenuViewModel);
+        }
+    }
+}
